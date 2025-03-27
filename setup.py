3a92d94445c54f9d26e044e8ff846da605691f9d
GersteinJo <71694809+GersteinJo@users.noreply.github.com>
2025-03-27 18:58:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cat_mod",  # Name of the library
-    version="0.1",  # Version number
+    version="0.2",  # Version number
     packages=find_packages(),  # Find all the Python packages inside the library
     install_requires=[  # Any external libraries your package depends on
         'numpy',
