7ed540221450f996cea2d96dfe059ea92c688326
GersteinJo <71694809+GersteinJo@users.noreply.github.com>
2025-05-13 02:31:28 +0300

    Update metric_modules.py

--- metric_modules.py
+++ metric_modules.py
@@ -5,32 +5,36 @@ import numpy as np
 import torch
 from scipy.stats import pearsonr
 
-def train_classifier(config, peck_obs_num = 1000, n_iter = 10000, encoded_data=None, labels=None, logger = None):
+def train_classifier(config, peck_obs_num = 200, n_iter = 10000, encoded_data=None, labels=None, logger = None):
     shuffle_mask = np.arange(len(labels))
     np.random.shuffle(shuffle_mask)
 
     sep = SEP.SEP(config['num_exemp'], output_space_shape=2, input_space_shape = 64,
-    lr = config['lr'], dr=config['dr'], omega = config['omega'], delta = config['delta']) # make kernel gaussian
+    lr = config['lr'], dr=config['dr'], omega = config['omega'], delta = config['delta'])
     results = np.zeros(peck_obs_num)
-    peck = True
+    peck = [1]
     counter = 0
 
     assert n_iter < len(labels), "Number of iterations should not surpass number of datapoints!"
     for i in range(n_iter):
-        obs, label = encoded_data[shuffle_mask[i]].reshape(1,-1), labels[shuffle_mask[i]].reshape(1,-1)
-        peck = sep.predict(obs)
         
-        if peck[0]==1 and not (logger is None):
-            logger.log(
-                {"peck_edible": label.item()},
-                step = counter
-                )
+        obs, label = encoded_data[shuffle_mask[i]].reshape(1,-1), labels[shuffle_mask[i]:shuffle_mask[i]+1]
+        # print(label)
+        peck = sep.predict(obs)
+
+        if peck[0]==1:# and not (logger is None):
+            # print("HEHEHEHEHEHHHHEHHEHEHEHEHEHEH")
+            if logger:
+                logger.log(
+                    {"peck_edible": label[0]},
+                    step = counter
+                    )
             results[counter] = label.item()
             counter += 1
-            if counter == peck_obs_num: print(f"Trial finished!"); break
-        if peck:
+            if counter == peck_obs_num: print(f"Trial {i} finished!"); break
             sep.fit(obs, label)
-        elif np.random.random() < 0.01:
+        elif np.random.random() < 0.05:
+            # print(f"label {label}")
             sep.fit(obs, label)
 
 
@@ -44,7 +48,7 @@ def encode_dataset(encoder, dataloader, orig_loader, device='cpu'):
         device: 'cpu' or 'cuda'
         
     Returns:
-        Tuple of (encoded_features_numpy, labels_numpy)
+        Tuple of numpy arrays (encoded_features_numpy, labels_numpy, original_images_numpy)
     """
     all_embeddings = []
     all_labels = []
@@ -76,7 +80,7 @@ def compare_embeddings(emb1, emb2):
     triu_indices = np.triu_indices_from(dissim1, k=1)
     flat1 = dissim1[triu_indices]
     flat2 = dissim2[triu_indices]
-    
+
     # Compute second-order similarity (Pearson correlation)
     r, _ = pearsonr(flat1, flat2)
     return r
