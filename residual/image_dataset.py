d2929e8303798ef873731a512ff10b69fd9a5a16
GersteinJo <71694809+GersteinJo@users.noreply.github.com>
2025-05-13 18:27:11 +0300

    Create image_dataset.py

---
+++ residual/image_dataset.py
@@ -0,0 +1,25 @@
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split, SubsetRandomSampler
+import pandas as pd
+from PIL import Image
+import numpy as np
+
+class ImageLabelDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.labels = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        img_name = self.labels.iloc[idx, 0]  # Assuming first column is image names
+        img_path = f"{self.img_dir}/{img_name}.png"
+        image = Image.open(img_path).convert('RGB')
+        label = self.labels.iloc[idx, 1]  # Assuming second column is labels
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
